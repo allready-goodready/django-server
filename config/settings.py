2ccadcd1bbd51d72bf992230c58021c249b0f83c
streqmin <sanghoo1023@gmail.com>
2025-05-27 11:49:47 +0900

    DB 설정 변경 및 환경변수 참조 추가

--- config/settings.py
+++ config/settings.py
@@ -10,11 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# 환경변수 초기화 (이미 해두셨다면 아래 두 줄은 생략)
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
