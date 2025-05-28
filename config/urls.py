5511dc035b2c3b090e7a06cc53a19cc4c7908a10
streqmin <sanghoo1023@gmail.com>
2025-05-28 17:37:10 +0900

    feat: 여행 계획 기능을 제공하는 api 경로 라우팅

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/plan", include("planner.urls", namespace="planner_api")),
 ]
