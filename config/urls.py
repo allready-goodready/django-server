8210e9e5af26061969e4707fb34802f4523e274b
Fellas <neuneu12@naver.com>
2025-05-28 17:23:05 +0900

    feat: url패턴 추가

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from accounts import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("base/", views.base, name='base'),
 ]
