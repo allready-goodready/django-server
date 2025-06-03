a27751460b01a7c65b44078dcd43c0262a7b66d5
streqmin <sanghoo1023@gmail.com>
2025-06-03 16:41:26 +0900

    feat: 여행 목적지 추가 및 수정을 요청하는 url 경로 추가

--- planner/urls.py
+++ planner/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import TravelPlanViewSet
+from .views import TravelPlanViewSet, DestinationViewSet
 
 app_name = "planner"
 
 router = DefaultRouter()
 router.register("travelplan", TravelPlanViewSet, basename="travelplan")
+router.register("destination", DestinationViewSet, basename="destination")
 
 urlpatterns = [
     path("", include(router.urls)),
