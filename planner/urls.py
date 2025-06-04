10696c9a2d2dc89e87acfabe72f87af36d7332d3
streqmin <sanghoo1023@gmail.com>
2025-06-04 09:42:42 +0900

    feat: 여행 출발지 저장 및 조회를 위한 url 경로 등록

--- planner/urls.py
+++ planner/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import TravelPlanViewSet, DestinationViewSet
+from .views import TravelPlanViewSet, DestinationViewSet, OriginViewSet
 
 app_name = "planner"
 
 router = DefaultRouter()
 router.register("travelplan", TravelPlanViewSet, basename="travelplan")
 router.register("destination", DestinationViewSet, basename="destination")
+router.register("origin", OriginViewSet, basename="origin")
 
 urlpatterns = [
     path("", include(router.urls)),
