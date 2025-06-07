a7e32773fdeb4db20c98a77383f777c6450b179b
sua <dssua0714@naver.com>
2025-06-07 22:36:06 +0900

    fix: admin 등록 위치를 feed앱으로 이동

--- api/models.py
+++ api/models.py
@@ -1,13 +1,2 @@
 from django.db import models
 
-from .models import Feed, FeedImage
-
-# Create your models here.
-@admin.register(Feed)
-class FeedAdmin(admin.ModelAdmin):
-    list_display = ('id', 'caption', 'place', 'created_at') # user 빠져있음
-    search_fields = ('caption', 'place')
-
-@admin.register(FeedImage)
-class FeedImageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'feed', 'order')
--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Feed, FeedImage
+
 
 # Register your models here.
+
+@admin.register(Feed)
+class FeedAdmin(admin.ModelAdmin):
+    list_display = ('id', 'caption', 'place', 'created_at') # user 빠져있음
+    search_fields = ('caption', 'place')
+
+@admin.register(FeedImage)
+class FeedImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'feed', 'order')
