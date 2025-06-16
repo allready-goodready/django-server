8e5ba60ace600baad79d868c35b8231e5561e5ce
streqmin <sanghoo1023@gmail.com>
2025-06-16 17:49:32 +0900

    feat: flight 앱의 url 파일에 app_name 추가

--- flight/urls.py
+++ flight/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import AirportNearOriginAPIView, AirportNearDestAPIView
 
+app_name = "flight"
+
 urlpatterns = [
-    # 출발지 근처 공항 조회
     path(
         "airports/from/",
         AirportNearOriginAPIView.as_view(),
         name="api_flight_airport_near_origin",
     ),
-    # 도착지 근처 공항 조회
     path(
         "airports/to/",
         AirportNearDestAPIView.as_view(),
